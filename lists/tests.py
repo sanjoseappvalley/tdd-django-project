b7823e35e5dfc1576d68598089b78b62a40f6e1d
My name <me@example.com>
2020-11-29 12:33:20 -0800

    First unit test and url mapping, dummy view

--- lists/tests.py
+++ lists/tests.py
@@ -1,9 +1,10 @@
 from django.test import TestCase
-
-# Create your tests here.
+from django.urls import resolve
+from lists.views import home_page
 
 
 class SmokeTest(TestCase):
 
-    def test_bad_math(self):
-        self.assertEqual(1 + 1, 3)
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
