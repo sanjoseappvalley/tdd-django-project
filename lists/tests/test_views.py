360d3822e2e060e42a0b5b16e408cf2996b2f44e
My name <me@example.com>
2020-12-27 18:33:59 -0800

    use new form in home_page, simplify tests. NB breaks stuff

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -25,6 +25,10 @@ class HomePageTest(TestCase):
         # self.assertEqual(html, expected_html)
         self.assertTemplateUsed(response, 'home.html')
 
+    def test_home_page_uses_item_form(self):
+        response = self.client.get('/')
+        self.assertIsInstance(response.context['form'], ItemForm)
+
 
 class NewListTest(TestCase):
 
