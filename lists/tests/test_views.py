87b08183bc919d9365e24be64354d689881970a6
My name <me@example.com>
2021-01-05 23:17:52 -0800

    add namespace for initialize function

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,11 +1,7 @@
 from django.test import TestCase
-# from django.urls import resolve
-# from django.http import HttpRequest
-#
-# from lists.views import home_page
 from lists.models import Item, List
 from django.utils.html import escape
-from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from lists.forms import ItemForm, EMPTY_ITEM_ERROR, DUPLICATE_ITEM_ERROR, ExistingListItemForm
 from unittest import skip
 
 
@@ -144,13 +140,12 @@ class ListViewTest(TestCase):
 
     def test_for_invalid_input_passes_form_to_template(self):
         response = self.post_invalid_input()
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
 
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
 
-    @skip
     def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
         list1 = List.objects.create()
         item1 = Item.objects.create(list=list1, text='textey')
@@ -158,7 +153,7 @@ class ListViewTest(TestCase):
             f'/lists/{list1.id}/',
             data={'text': 'textey'}
         )
-        expected_error = escape("You've already got this in your list")
+        expected_error = escape(DUPLICATE_ITEM_ERROR)
         self.assertContains(response, expected_error)
         self.assertTemplateUsed(response, 'list.html')
         self.assertEqual(Item.objects.all().count(), 1)
@@ -166,5 +161,5 @@ class ListViewTest(TestCase):
     def test_displays_item_form(self):
         list_ = List.objects.create()
         response = self.client.get(f'/lists/{list_.id}/')
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
         self.assertContains(response, 'name="text"')
--- lists/views.py
+++ lists/views.py
@@ -2,22 +2,11 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from lists.models import Item, List
 from django.core.exceptions import ValidationError
-from lists.forms import ItemForm
+from lists.forms import ExistingListItemForm, ItemForm
 
 
 def home_page(request):
     return render(request, 'home.html', {'form': ItemForm()})
-
-
-def view_list(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    form = ItemForm()
-    if request.method == 'POST':
-        form = ItemForm(data=request.POST)
-        if form.is_valid():
-            form.save(for_list=list_)
-            return redirect(list_)
-    return render(request, 'list.html', {'list': list_, "form": form})
 
 
 def new_list(request):
@@ -28,6 +17,18 @@ def new_list(request):
         return redirect(list_)
     else:
         return render(request, 'home.html', {'form': form})
+
+
+def view_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    form = ExistingListItemForm(for_list=list_)
+    if request.method == 'POST':
+        form = ExistingListItemForm(for_list=list_, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(list_)
+    return render(request, 'list.html', {'list': list_, "form": form})
+
 
 #
 # def add_item(request, list_id):
